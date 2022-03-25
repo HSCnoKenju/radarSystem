16a809a26778262f017daf391f5a6f1302bc261c
HSCnoKenju <26322756+HSCnoKenju@users.noreply.github.com>
2022-03-25 12:58:31 +0100

    deviceFactory and factory methods for led, sonar

--- it.unibo.radarSystem22.domain/src/it/unibo/radarSystem22/domain/model/LedModel.java
+++ it.unibo.radarSystem22.domain/src/it/unibo/radarSystem22/domain/model/LedModel.java
@@ -1,23 +1,50 @@
 package it.unibo.radarSystem22.domain.model;
 
+import it.unibo.radarSystem22.domain.concrete.LedConcrete;
 import it.unibo.radarSystem22.domain.interfaces.ILed;
+import it.unibo.radarSystem22.domain.mock.LedMock;
+import it.unibo.radarSystem22.domain.mock.LedMockWithGUI;
+import it.unibo.radarSystem22.domain.utils.ColorsOut;
+import it.unibo.radarSystem22.domain.utils.DomainSystemConfig;
 
 public abstract class LedModel implements ILed {
-	
-	private boolean state = false; 
 
-	
-	protected abstract void ledActivate (boolean value);
-	
-	protected void setState (boolean value) {
-		state = value ;
+	private boolean state = false;
+
+	public static ILed create() {
+		ILed led;
+		if (DomainSystemConfig.simulation)
+			led = createLedMock();
+		else
+			led = createLedConcrete();
+		return led;
+	}
+
+	public static ILed createLedMock() {
+		ColorsOut.out("DeviceFactory | createLedMock ledGui=" + DomainSystemConfig.ledGui, ColorsOut.GREEN);
+		if (DomainSystemConfig.ledGui)
+			return new LedMockWithGUI();
+		else
+			return new LedMock();
+
+	}
+
+	public static ILed createLedConcrete() {
+		ColorsOut.out("createLedConcrete", ColorsOut.BLUE);
+		return new LedConcrete();
+	}
+
+	protected abstract void ledActivate(boolean value);
+
+	protected void setState(boolean value) {
+		state = value;
 		ledActivate(value);
 	}
 
 	@Override
 	public void turnOn() {
 		setState(true);
-		
+
 	}
 
 	@Override
--- it.unibo.radarSystem22.domain/src/it/unibo/radarSystem22/domain/model/SonarModel.java
+++ it.unibo.radarSystem22.domain/src/it/unibo/radarSystem22/domain/model/SonarModel.java
@@ -1,12 +1,31 @@
 package it.unibo.radarSystem22.domain.model;
 
+import it.unibo.radarSystem22.domain.concrete.SonarConcrete;
 import it.unibo.radarSystem22.domain.interfaces.IDistance;
 import it.unibo.radarSystem22.domain.interfaces.ISonar;
+import it.unibo.radarSystem22.domain.mock.SonarMock;
+import it.unibo.radarSystem22.domain.utils.ColorsOut;
+import it.unibo.radarSystem22.domain.utils.DomainSystemConfig;
 
 public abstract class SonarModel implements ISonar {
 
 	protected boolean stopped = false;
 	protected IDistance currentDistance = new Distance(90);
+	
+	
+	public static ISonar create() {
+		if( DomainSystemConfig.simulation )  return createSonarMock();
+		else  return createSonarConcrete();		
+	}
+
+	public static ISonar createSonarMock() {
+		ColorsOut.out("createSonarMock", ColorsOut.BLUE);
+		return new SonarMock();
+	}	
+	public static ISonar createSonarConcrete() {
+		ColorsOut.out("createSonarConcrete", ColorsOut.BLUE);
+		return new SonarConcrete();
+	}	
 
 	protected SonarModel() {
 		super();
