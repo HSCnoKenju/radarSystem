b9892cf88cfa72485a351dffb8524b94dd47533c
HSCnoKenju <26322756+HSCnoKenju@users.noreply.github.com>
2022-03-27 22:32:06 +0200

    wire Led and Radar with an Controller

---
+++ it.unibo.radarSystem22/src/it/unibo/radarSystem22/IApplication.java
@@ -0,0 +1,7 @@
+package it.unibo.radarSystem22;
+
+public interface IApplication {
+	public void doJob(String configFileName, String systemConfig);
+
+	public String getName();
+}
---
+++ it.unibo.radarSystem22/src/it/unibo/radarSystem22/sprint1/usecases/RadarGuiUsecase.java
@@ -0,0 +1,13 @@
+package it.unibo.radarSystem22.sprint1.usecases;
+
+import it.unibo.radarSystem22.domain.interfaces.IDistance;
+import it.unibo.radarSystem22.domain.interfaces.IRadarDisplay;
+
+public class RadarGuiUsecase {
+
+	public static void doUseCase(IRadarDisplay radar, IDistance d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
