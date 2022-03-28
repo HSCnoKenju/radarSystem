9ab56efccfabf427a2b48f357fa03f2778040b68
HSCnoKenju <26322756+HSCnoKenju@users.noreply.github.com>
2022-03-28 20:21:21 +0200

    TcpSupport oneshot connection

--- it.unibo.comm2022/src/it/unibo/comm2022/tcp/TcpClientSupport.java
+++ it.unibo.comm2022/src/it/unibo/comm2022/tcp/TcpClientSupport.java
@@ -1,25 +1,28 @@
 package it.unibo.comm2022.tcp;
+
 import java.net.Socket;
 import it.unibo.comm2022.interfaces.Interaction2021;
 import it.unibo.comm2022.utils.ColorsOut;
 
-
 public class TcpClientSupport {
 
-	
-	public static Interaction2021 connect(String host, int port, int nattempts ) throws Exception {		 
-		for( int i=1; i<=nattempts; i++ ) {
+	public static Interaction2021 connect(String host, int port, int nattempts) throws Exception {
+		for (int i = 1; i <= nattempts; i++) {
 			try {
-				Socket socket         =  new Socket( host, port );
- 				Interaction2021 conn  =  new TcpConnection( socket );
+				Socket socket = new Socket(host, port);
+				Interaction2021 conn = new TcpConnection(socket);
 				return conn;
-			}catch(Exception e) {
+			} catch (Exception e) {
 				ColorsOut.out("TcpClient | Another attempt to connect with host:" + host + " port=" + port);
 				Thread.sleep(500);
 			}
-		}//for
+		} // for
 		throw new Exception("TcpClient | Unable to connect to host:" + host);
-		 
- 	}
- 
+
+	}
+
+	public static Interaction2021 connect(String host, int port) throws Exception {
+		return connect(host, port, 1);
+	}
+
 }
