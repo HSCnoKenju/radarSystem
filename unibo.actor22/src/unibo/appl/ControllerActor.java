53b4095851f6dc950cfaedcedb80d544762c9b9d
HSCnoKenju <26322756+HSCnoKenju@users.noreply.github.com>
2022-04-10 17:06:30 +0200

    distribution-ready, use concrete class on raspberry
    create distribution
    
    > ./gradlew distZip
    
    configuration files are ready as-written
    
    need to copy into /bin directory script for the domain (sonar and led)

---
+++ unibo.actor22/src/unibo/appl/ControllerActor.java
@@ -0,0 +1,121 @@
+package unibo.appl;
+
+import it.unibo.kactor.IApplMessage;
+import unibo.actor22.QakActor22;
+import unibo.actor22comm.utils.ColorsOut;
+import unibo.actor22comm.utils.CommUtils;
+
+/*
+ * Il controller conosce SOLO I NOMI dei dispositivi
+ * (non ha riferimenti ai dispositivi-attori)
+ */
+public class ControllerActor extends QakActor22 {
+    protected int numIterLed = 1;
+    protected int numIterSonar = 0;
+    protected IApplMessage getStateRequest;
+    protected IApplMessage getSonarActiveRequest;
+    protected IApplMessage getDistanceRequest;
+    protected boolean on = true;
+
+    public ControllerActor(String name) {
+        super(name);
+        getStateRequest = ApplData.buildRequest(name, "ask", ApplData.reqLedState, ApplData.ledName);
+        getSonarActiveRequest = ApplData.buildRequest(name, "ask", ApplData.reqSonarIsActive, ApplData.sonarName);
+        getDistanceRequest = ApplData.buildRequest(name, "ask", ApplData.reqSonarGetDistance, ApplData.sonarName);
+
+    }
+
+    @Override
+    protected void handleMsg(IApplMessage msg) {
+        if (msg.isReply()) {
+            elabAnswer(msg);
+        } else {
+            elabCmd(msg);
+        }
+    }
+
+    protected void elabCmd(IApplMessage msg) {
+        String msgCmd = msg.msgContent();
+        ColorsOut.outappl(getName() + " | elabCmd=" + msgCmd, ColorsOut.GREEN);
+        switch (msgCmd) {
+            case ApplData.cmdActivate: {
+               doControllerWorkLed();
+                doControllerWorkSonar();
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    protected void wrongBehavior() {
+        //WARNING: Inviare un treno di messaggi VA EVITATO
+        //mantiene il controllo del Thread degli attori (qaksingle)
+        for (int i = 1; i <= 3; i++) {
+            forward(ApplData.turnOffLed);
+            CommUtils.delay(500);
+            forward(ApplData.turnOnLed);
+            CommUtils.delay(500);
+        }
+        forward(ApplData.turnOffLed);
+    }
+
+    protected void doControllerWorkLed() {
+        CommUtils.aboutThreads(getName() + " |  Before doControllerWorkLed on=" + on);
+        //wrongBehavior();
+        ColorsOut.outappl( getName()  + " | numIterLed=" + numIterLed  , ColorsOut.GREEN);
+        if (numIterLed++ < 5) { // modifico l'iterazione ad ogni esecuzione
+            if (numIterLed % 2 == 1) forward(ApplData.turnOnLed); //accesione
+            else forward(ApplData.turnOffLed); //spegnimento
+            request(getStateRequest);
+        } else {
+            forward(ApplData.turnOffLed);
+            //ColorsOut.outappl(getName() + " | emit " + ApplData.endWorkEvent, ColorsOut.MAGENTA);
+            //emit( ApplData.endWorkEvent );
+        }
+
+    }
+
+    protected void doControllerWorkSonar(){
+        CommUtils.aboutThreads(getName() + " |  Before doControllerWorkSonar on=" + on);
+        numIterSonar++;
+        ColorsOut.outappl( getName()  + " | numIterSonar=" + numIterSonar  , ColorsOut.GREEN);
+
+        if (numIterSonar == 1 ){
+            forward(ApplData.sonarActivate);
+            request(getSonarActiveRequest);
+
+        }
+        else if ( numIterSonar < 5) {
+            request(getDistanceRequest);
+        }
+        else {
+            forward(ApplData.sonarDeactivate);
+            request(getSonarActiveRequest);
+        }
+
+    }
+
+
+    protected void elabAnswer(IApplMessage msg) {
+        ColorsOut.outappl(getName() + " | elabAnswer " + " " + msg, ColorsOut.MAGENTA);
+        CommUtils.delay(300);
+
+        String sender = msg.msgSender();
+        switch (sender){
+
+            case ApplData.ledName:
+                doControllerWorkLed(); // ogni volta che ricevo il messaggio, rieseguo l'invio del comando
+                break;
+
+            case ApplData.sonarName:
+                doControllerWorkSonar();
+                break;
+
+            default:
+                ColorsOut.outerr(getName() + " | unknown " + sender);
+
+        }
+    }
+
+}
--- unibo.actor22/src/unibo/main/RSActor22onPC.java
+++ unibo.actor22/src/unibo/main/RSActor22onPC.java
@@ -1,32 +1,31 @@
-package unibo.actor22.local;
+package unibo.main;
 
-import it.unibo.radarSystem22.domain.utils.BasicUtils;
 import it.unibo.radarSystem22.domain.utils.DomainSystemConfig;
 import unibo.actor22.Qak22Util;
-import unibo.actor22.common.ApplData;
-import unibo.actor22.common.ControllerActor;
-import unibo.actor22.common.LedActor;
-import unibo.actor22.common.SonarActor;
 import unibo.actor22comm.utils.ColorsOut;
 import unibo.actor22comm.utils.CommSystemConfig;
 import unibo.actor22comm.utils.CommUtils;
+import unibo.appl.ApplData;
+import unibo.appl.ControllerActor;
+import unibo.appl.LedActor;
+import unibo.appl.SonarActor;
 
 
 /*
  * Sistema che usa led e controller come attori locali
  */
 
-public class UsingLedAndControllerOnPc {
+public class RSActor22onPC {
 
 
     public static void main(String[] args) {
         CommUtils.aboutThreads("Before start - ");
-        new UsingLedAndControllerOnPc().doJob();
+        new RSActor22onPC().doJob();
         CommUtils.aboutThreads("Before end - ");
     }
 
     public void doJob() {
-        ColorsOut.outappl("ControllerUsingLedOnPc | Start", ColorsOut.BLUE);
+        ColorsOut.outappl(getClass().getName() + "| Start", ColorsOut.BLUE);
         configure();
         CommUtils.aboutThreads("Before execute - ");
         //CommUtils.waitTheUser();
@@ -37,20 +36,14 @@ public class UsingLedAndControllerOnPc {
     protected void configure() {
         DomainSystemConfig.simulation = true;
         DomainSystemConfig.ledGui = true;
-        DomainSystemConfig.tracing = false;
-        CommSystemConfig.tracing = false;
+        DomainSystemConfig.tracing = true;
+        CommSystemConfig.tracing = true;
 
         new LedActor(ApplData.ledName);
         new SonarActor(ApplData.sonarName);
         new ControllerActor(ApplData.controllerName);
 
-        //Creo altri Led per verificare che il numero di thread non aumenta
-		/*for( int i=1; i<=3; i++) {
-			new LedActor(ApplData.ledName+"_"+i);
-			Qak22Util.sendAMsg(ApplData.turnOnLed, ApplData.ledName+"_"+i  );
-			BasicUtils.delay(500);
-			Qak22Util.sendAMsg(ApplData.turnOffLed, ApplData.ledName+"_"+i  );
-		}*/
+
     }
 
 
@@ -61,7 +54,7 @@ public class UsingLedAndControllerOnPc {
 
     public void terminate() {
         CommUtils.aboutThreads("Before exit - ");
-        CommUtils.delay(3000);
+        CommUtils.delay(3000); // quando muore tutto
         System.exit(0);
     }
 
